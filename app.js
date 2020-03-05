a8f7c3395857bf16508d03f873419ec8ea3e42a0
Filipe Sousa <filipesousamail@gmail.com>
2020-03-05 17:28:33 +0000

    Added debounce feature to scroll to top

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY - this.innerHeight > 100) {
+    $(window).scroll(debounce(function () {
+        if (this.scrollY > this.innerHeight) {
             $('#back-to-top').addClass('show');
         } else {
             $('#back-to-top').removeClass('show');
         }
-    });
+    }, 250));
 });
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
